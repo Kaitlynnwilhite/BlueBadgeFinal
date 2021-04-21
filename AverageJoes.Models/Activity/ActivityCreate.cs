6e78451d5f670ec138d62d96d36bdec6d1ea3e7b
kaitlynnwilhite <kaitlynn.wilhite1@gmail.com>
2021-04-21 11:26:41 -0400

    activity models

--- AverageJoes.Models/Activity/ActivityCreate.cs
+++ AverageJoes.Models/Activity/ActivityCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace AverageJoes.Models.Activity
 {
     public class ActivityCreate
     {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Descripton { get; set; }
+        [Required]
+        public Guid OwnerID { get; set; }
     }
 }
+
--- AverageJoes.Models/Activity/ActivityEdit.cs
+++ AverageJoes.Models/Activity/ActivityEdit.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace AverageJoes.Models.Activity
 {
     public class ActivityEdit
     {
+        [Key]
+        
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+     
+        public string Descripton { get; set; }
+    
+
+        
     }
 }
+
