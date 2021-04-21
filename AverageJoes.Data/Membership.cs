d99ffae247a3427e8b474ca23bd31eb91ebe26a2
jeromedawn <jeromedawntraining@gmail.com>
2021-04-21 09:48:39 -0400

    memberships

--- AverageJoes.Data/Membership.cs
+++ AverageJoes.Data/Membership.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AverageJoes.Data
 {
-    public class Membership
+    public class Memberships
     {
         public enum MembershipType
         {
@@ -20,7 +20,11 @@ namespace AverageJoes.Data
         public Guid OwnerID { get; set; }
         [Required]
 
-        public MembershipType membership { get; set; }
+        public MembershipType MembershipTypes { get; set; }
+
+        [Required]
+
+        public string Notes { get; set; }
 
         [Required]
 
@@ -28,7 +32,6 @@ namespace AverageJoes.Data
 
         public DateTimeOffset? ModifiedUtc { get; set; }
 
-        //I am making changes okay github
 
     }
 }
